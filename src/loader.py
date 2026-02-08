f3ad35d61fc3279d8f968c4835f7170fac0b5342
Maaz Ahmed <maazahmad3838@gmail.com>
2026-02-08 23:31:30 +0500

    Improvement in indentation

--- src/loader.py
+++ src/loader.py
@@ -2,40 +2,40 @@ import csv
 import os
 
 def load_data(filepath):
-
+    # file exists?
     if not os.path.exists(filepath):
         raise FileNotFoundError("File not found.")
 
     raw_data = []
 
-    try:
-        with open(filepath, mode='r', encoding='utf-8') as f:
-            reader = csv.DictReader(f)
-            for row in reader:
-                raw_data.append(row)
-
-    except Exception:
-        raise IOError("Error reading CSV file.")
+    # open file and read as rows
+    with open(filepath, mode='r', encoding='utf-8') as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            raw_data.append(row)
 
     return clean_data(raw_data)
 
 
 def clean_data(data):
-
     if not data:
         return []
 
+    # find all columns with years
     years = [key for key in data[0].keys() if key.isdigit()]
 
     cleaned = []
 
+    # loop through each country row
     for row in data:
         for year in years:
             value = row.get(year)
 
+            # skip empty values
             if value and value.strip() != "":
+                # removing commas
                 value = value.replace(",", "")
-
+                
                 cleaned.append({
                     "Country": row["Country Name"],
                     "Region": row["Continent"],
@@ -44,4 +44,3 @@ def clean_data(data):
                 })
 
     return cleaned
-
