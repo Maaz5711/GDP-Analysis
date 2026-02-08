60aa9d356e2fd33a4226661900e56687c7d4689f
MUHAMMAD HUZAIFA <chambelli303@gmail.com>
2026-02-08 23:35:25 +0500

    Add comments

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import loader
 import dashboard
 
-DATA_FILE = "gdp.csv"
+DATA_FILE = "gdp.csv" #name of the data file
 
 data = loader.load_data(DATA_FILE)
 regions = dashboard.get_all_regions(data)
 year = dashboard.get_latest_year(data)
 
-print("Launching interactive dashboard...")
+print("Launching interactive dashboard...") #show output in a seperate window
 dashboard.create_dashboard(data, regions, year)
