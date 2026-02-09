b2a8e9159774b9cf0e21857f30a5d85d83e978d5
Maaz Ahmed <maazahmad3838@gmail.com>
2026-02-09 13:42:04 +0500

    change description

--- main.py
+++ main.py
@@ -7,5 +7,5 @@ data = loader.load_data(DATA_FILE)
 regions = dashboard.get_all_regions(data)
 year = dashboard.get_latest_year(data)
 
-print("Launching interactive dashboard...") #show output in a seperate window
+print("------Loading dashboard------") #show output in a seperate window
 dashboard.create_dashboard(data, regions, year)
